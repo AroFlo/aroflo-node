b823ec4cc04d1e63d89ad0675162c3e365db91cf
AroFlo Developers <devops@aroflo.com>
2021-03-10 23:39:10 +0000

    Compiling Library for API version 2021.3.17

---
+++ src/operations/reporting/trackingCentres/index.js
@@ -0,0 +1,46 @@
+/**
+ * This module was automatically generated from the OpenAPI specification
+ * of the AroFlo Extended API located here: https://api.aroflo.com/openapi
+ */
+
+
+module.exports = (AroFlo) => ({
+	/**
+	 * Get Tracking Centres.
+	 * 
+	 * Obtains details about the specific tracking centre.
+	 */
+	get: async (trackingcentre) => {
+		// validate path parameters
+		if (trackingcentre === undefined || trackingcentre == null) {
+			throw new Error("`trackingcentre` is a required argument and must be provided a non-null value.");
+		}
+		
+		return AroFlo.httpRequest.get(`/reporting/trackingcentres/${trackingcentre}`, {});
+	},
+	
+	/**
+	 * List Tracking centres.
+	 * 
+	 * Collection of Tracking Centre Details.
+	 */
+	list: async (query = {}) => {
+		// parse the query string components
+		const urlSearchParams = new URLSearchParams();
+		if (typeof query != "undefined" && query != null) {
+			if (typeof query != "object") {
+				throw new Error("`query` argument is not of type Object.");
+			}
+		
+			const flatQuery = AroFlo.utils.flattenObjectIntoDotNotation(query);
+			Object.keys(flatQuery)
+					.filter(_key => flatQuery.hasOwnProperty(_key))
+					.forEach(_key => urlSearchParams.append(_key, Array.isArray(flatQuery[_key])
+							? flatQuery[_key].join(",")
+							: flatQuery[_key]));
+			urlSearchParams.sort()
+		}
+		
+		return AroFlo.httpRequest.get(`/reporting/trackingcentres?` + urlSearchParams.toString(), {});
+	}
+});
