2e3b1605e67ea8e2ddff914134d366bf7939f8f4
AroFlo Developers <devops@aroflo.com>
2021-03-17 02:50:19 +0000

    Compiling Library for API version 2021.3.28

---
+++ src/operations/compliance/index.js
@@ -0,0 +1,9 @@
+/**
+ * This module was automatically generated from the OpenAPI specification
+ * of the AroFlo Extended API located here: https://api.aroflo.com/openapi
+ */
+const __ALERTS = require('./alerts');
+
+module.exports = (AroFlo) => ({
+	alerts: __ALERTS(AroFlo)
+});
