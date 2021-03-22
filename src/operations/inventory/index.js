0ad38d61f55a8697cd46fa523bed649be139b4d1
AroFlo Developers <devops@aroflo.com>
2021-03-22 23:12:36 +0000

    Compiling Library for API version 2021.3.41

---
+++ src/operations/inventory/index.js
@@ -0,0 +1,9 @@
+/**
+ * This module was automatically generated from the OpenAPI specification
+ * of the AroFlo Extended API located here: https://api.aroflo.com/openapi
+ */
+const __CUSTOMHOLDERS = require('./customHolders');
+
+module.exports = (AroFlo) => ({
+	customHolders: __CUSTOMHOLDERS(AroFlo)
+});
