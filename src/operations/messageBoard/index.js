b5f15628f0d875536f2fa9d334900f0678d619c7
AroFlo Developers <devops@aroflo.com>
2021-04-12 04:39:38 +0000

    Compiling Library for API version 2021.4.17

---
+++ src/operations/messageBoard/index.js
@@ -0,0 +1,9 @@
+/**
+ * This module was automatically generated from the OpenAPI specification
+ * of the AroFlo Extended API located here: https://api.aroflo.com/openapi
+ */
+const __MESSAGES = require('./messages');
+
+module.exports = (AroFlo) => ({
+	messages: __MESSAGES(AroFlo)
+});
