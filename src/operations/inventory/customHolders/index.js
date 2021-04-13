d6723a2c71fc8c10722d18a9a52842196cfdc8eb
AroFlo Developers <devops@aroflo.com>
2021-04-13 02:56:33 +0000

    Compiling Library for API version 2021.4.23

---
+++ src/operations/inventory/customHolders/index.js
@@ -0,0 +1,52 @@
+/**
+ * This module was automatically generated from the OpenAPI specification
+ * of the AroFlo Extended API located here: https://api.aroflo.com/openapi
+ */
+
+
+module.exports = (AroFlo) => ({
+	/**
+	 * Retrieve a Custom Holder.
+	 * 
+	 * Retrieves the specified Custom Holder.
+	 */
+	get: async (customholder) => {
+		// validate path parameters
+		if (customholder === undefined || customholder == null) {
+			throw new Error("`customholder` is a required argument and must be provided a non-null value.");
+		}
+		
+		return AroFlo.httpRequest.get(`/inventory/customholders/${customholder}`, {});
+	},
+	
+	/**
+	 * List Custom Holders.
+	 * 
+	 * Returns details about a collection of Custom Holders.
+	 */
+	list: async (query = {}) => {
+		// parse the query string components
+		const urlSearchParams = [];
+		if (typeof query != "undefined" && query != null) {
+			if (typeof query != "object") {
+				throw new Error("`query` argument is not of type Object.");
+			}
+		
+			const flatQuery = AroFlo.utils.flattenObjectIntoDotNotation(query);
+			Object.keys(flatQuery)
+					.filter(_key => flatQuery.hasOwnProperty(_key))
+					.forEach(_key => {
+						let _value = flatQuery[_key];
+						_value = Array.isArray(_value) ? _value.join(",") : _value;
+						_value = _value.toString();
+						_value = _value.replace(/\%/g, "%25");
+						_value = _value.replace(/\&/g, "%26");
+						_value = _value.replace(/\#/g, "%23");
+						urlSearchParams.push(_key + "=" + _value);
+					});
+			urlSearchParams.sort()
+		}
+		
+		return AroFlo.httpRequest.get(`/inventory/customholders?` + urlSearchParams.toString(), {});
+	}
+});
---
+++ src/operations/priorities/index.js
@@ -0,0 +1,57 @@
+/**
+ * This module was automatically generated from the OpenAPI specification
+ * of the AroFlo Extended API located here: https://api.aroflo.com/openapi
+ */
+
+
+module.exports = (AroFlo) => ({
+	/**
+	 * Retrieve Priority.
+	 * 
+	 * Retrieves the specified Priority.
+	 */
+	get: async (priority) => {
+		// validate path parameters
+		if (priority === undefined || priority == null) {
+			throw new Error("`priority` is a required argument and must be provided a non-null value.");
+		}
+		
+		return AroFlo.httpRequest.get(`/priorities/${priority}`, {});
+	},
+	
+	/**
+	 * List Priorities.
+	 * 
+	 * Returns details about a collection of Priorities.
+	 * @param {object} payload Object to be posted as the body payload.
+	 */
+	list: async (query = {}, payload = {}) => {
+		// parse the query string components
+		const urlSearchParams = [];
+		if (typeof query != "undefined" && query != null) {
+			if (typeof query != "object") {
+				throw new Error("`query` argument is not of type Object.");
+			}
+		
+			const flatQuery = AroFlo.utils.flattenObjectIntoDotNotation(query);
+			Object.keys(flatQuery)
+					.filter(_key => flatQuery.hasOwnProperty(_key))
+					.forEach(_key => {
+						let _value = flatQuery[_key];
+						_value = Array.isArray(_value) ? _value.join(",") : _value;
+						_value = _value.toString();
+						_value = _value.replace(/\%/g, "%25");
+						_value = _value.replace(/\&/g, "%26");
+						_value = _value.replace(/\#/g, "%23");
+						urlSearchParams.push(_key + "=" + _value);
+					});
+			urlSearchParams.sort()
+		}
+		
+		if (typeof payload != "object") {
+			throw new Error("`payload` argument is not of type Object.");
+		}
+		
+		return AroFlo.httpRequest.get(`/priorities?` + urlSearchParams.toString(), {}, payload);
+	}
+});
