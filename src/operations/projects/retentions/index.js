42757f7428f0ece3de21d56a4584cd2cd94a9703
AroFlo Developers <devops@aroflo.com>
2021-07-15 02:01:36 +0000

    Removing files from previous version

--- src/operations/projects/retentions/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * This module was automatically generated from the OpenAPI specification
- * of the AroFlo Extended API located here: https://api.aroflo.com/openapi
- */
-const __SCHEMES = require('./schemes');
-
-module.exports = (AroFlo) => ({
-	schemes: __SCHEMES(AroFlo)
-});
