11ff94f85256e70e59a9e922ceb98e7b86441bc6
AroFlo Developers <devops@aroflo.com>
2021-08-10 02:05:26 +0000

    Removing files from previous version

--- src/operations/reporting/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * This module was automatically generated from the OpenAPI specification
- * of the AroFlo Extended API located here: https://api.aroflo.com/openapi
- */
-const __TRACKINGCENTRES = require('./trackingCentres');
-
-module.exports = (AroFlo) => ({
-	trackingCentres: __TRACKINGCENTRES(AroFlo)
-});
