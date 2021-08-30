8a420a0d696ba26e4044ebfae7d08c5fe137ad88
AroFlo Developers <devops@aroflo.com>
2021-08-30 06:42:17 +0000

    Removing files from previous version

--- src/operations/compliance/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * This module was automatically generated from the OpenAPI specification
- * of the AroFlo Extended API located here: https://api.aroflo.com/openapi
- */
-const __ALERTS = require('./alerts');
-
-module.exports = (AroFlo) => ({
-	alerts: __ALERTS(AroFlo)
-});
