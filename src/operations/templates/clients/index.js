09885d2fccfb4638c0bdf399b39c7b09d3fc9a66
AroFlo Developers <devops@aroflo.com>
2021-09-01 01:39:58 +0000

    Removing files from previous version

--- src/operations/templates/clients/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * This module was automatically generated from the OpenAPI specification
- * of the AroFlo Extended API located here: https://api.aroflo.com/openapi
- */
-const __CUSTOMFIELDS = require('./customfields');
-
-module.exports = (AroFlo) => ({
-	customfields: __CUSTOMFIELDS(AroFlo)
-});
