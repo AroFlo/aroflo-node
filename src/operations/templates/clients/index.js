e37111c6114babeb13917e555072d804e9878b61
AroFlo Developers <devops@aroflo.com>
2021-09-01 01:39:58 +0000

    Compiling Library for API version 2021.9.1

---
+++ src/operations/templates/clients/index.js
@@ -0,0 +1,9 @@
+/**
+ * This module was automatically generated from the OpenAPI specification
+ * of the AroFlo Extended API located here: https://api.aroflo.com/openapi
+ */
+const __CUSTOMFIELDS = require('./customfields');
+
+module.exports = (AroFlo) => ({
+	customfields: __CUSTOMFIELDS(AroFlo)
+});
