af48563762de0e514d594cdc6f7fb3aeafcd3b96
AroFlo Developers <devops@aroflo.com>
2021-10-06 02:32:31 +0000

    Removing files from previous version

--- src/operations/user/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * This module was automatically generated from the OpenAPI specification
- * of the AroFlo Extended API located here: https://api.aroflo.com/openapi
- */
-const __CREWS = require('./crews');
-
-module.exports = (AroFlo) => ({
-	crews: __CREWS(AroFlo)
-});
