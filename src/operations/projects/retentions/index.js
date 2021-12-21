522b800b6c6aa34a3ab5f0dc9a2ae3cf1187c51f
AroFlo Developers <devops@aroflo.com>
2021-12-21 10:23:28 +0000

    Compiling Library for API version 2021.12.1

---
+++ src/operations/projects/retentions/index.js
@@ -0,0 +1,9 @@
+/**
+ * This module was automatically generated from the OpenAPI specification
+ * of the AroFlo Extended API located here: https://api.aroflo.com/openapi
+ */
+const __SCHEMES = require('./schemes');
+
+module.exports = (AroFlo) => ({
+	schemes: __SCHEMES(AroFlo)
+});
